adf619096866ae41a19f2ce4df5375267a4ec192
Steve Mandes <mandessteve@gmail.com>
2017-10-20 13:05:05 -0400

    Updated Tabs

--- js/script.js
+++ js/script.js
@@ -1,32 +1,14 @@
+//TABS
 $(document).ready(function(){
 
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
 
-	//Select the first tab and div by default
-	$('#tab_nav > ul > li > a').eq(0).addClass( "selected" );
-	$('#tab_nav > div').eq(0).addClass( "selected" );
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
 
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
 
-  //EVENT DELEGATION
-	//This assigns an onclick event listener to the UL tag.
-  //Then it checks what A tag was selected.
-		$('#tab_nav > ul').on('click','a',function(){
-
-        var aElement = $('#tab_nav > ul > li > a');
-        var divContent = $('#tab_nav > div');
-
-         /*Handle Tab Nav*/
-			   aElement.removeClass( "selected");
-			   $(this).addClass( "selected");
-
-        /*Handle Tab Content*/
-			   var clicked_index = aElement.index(this);
-			   divContent.css('display','none');
-			   divContent.eq(clicked_index).css('display','block');
-
-		   	$(this).blur();
-			  return false;
-
-		});
-
-
-});//end ready
+})
