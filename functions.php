2be091fb2e4988d19873b53248329581710babcf
Brandon Diaz-Abreu <mail@fullstackman.net>
2018-01-26 16:52:55 -0500

    Testing out a custom meta box on the page editor.

--- functions.php
+++ functions.php
@@ -1,4 +1,89 @@
 <?php
+/**
+ * Basic way to add new meta box to the page screen
+ */
+ // https://code.tutsplus.com/tutorials/how-to-create-custom-wordpress-writemeta-boxes--wp-20336 
+add_action( 'add_meta_boxes', 'wpse44966_add_meta_box' );
+
+ 
+function wpse44966_add_meta_box()
+{
+    add_meta_box(
+        'wpse44966-meta-box', // id, used as the html id att
+        __( 'WPSE 44966 Meta Box' ), // meta box title, like "Page Attributes"
+        'wpse44966_meta_box_cb', // callback function, spits out the content
+        'page', // post type or page. We'll add this to pages only
+        'side', // context (where on the screen
+        'low' // priority, where should this go in the context?
+    );
+}
+
+//Callback function for our meta box.  Echos out the content
+function wpse44966_meta_box_cb( $post )
+{
+	$menus = get_terms('nav_menu');
+	echo("<script>console.log('PHP found these menus: ".json_encode($menus)."');</script>");
+	$nav_menus = wp_get_nav_menus();
+	// Set $nav_menu_selected_id to 0 if no menus.
+	if ( $one_theme_location_no_menus ) {
+	  $nav_menu_selected_id = 0;
+	} elseif ( empty( $nav_menu_selected_id ) && ! empty( $nav_menus ) && ! $add_new_screen ) {
+	  // if we have no selection yet, and we have menus, set to the first one in the list.
+	  $nav_menu_selected_id = $nav_menus[0]->term_id;
+	}
+	// Generate truncated menu names.
+	foreach ( (array) $nav_menus as $key => $_nav_menu ) {
+	  $nav_menus[$key]->truncated_name = wp_html_excerpt( $_nav_menu->name, 40, '&hellip;' );
+	}
+	if ( current_theme_supports( 'menus' ) ) {
+	  $locations = get_registered_nav_menus();
+	  $menu_locations = get_nav_menu_locations();
+	}
+	?>
+	<select name="menu" id="select-menu-to-edit">
+	<?php if ( $add_new_screen ) : ?>
+	  <option value="0" selected="selected"><?php _e( '&mdash; Select &mdash;' ); ?></option>
+	<?php endif; ?>
+	<?php foreach ( (array) $nav_menus as $_nav_menu ) : ?>
+	  <option value="<?php echo esc_attr( $_nav_menu->term_id ); ?>" <?php selected( $_nav_menu->term_id, $nav_menu_selected_id ); ?>>
+	    <?php
+	    echo esc_html( $_nav_menu->truncated_name ) ;
+
+	    if ( ! empty( $menu_locations ) && in_array( $_nav_menu->term_id, $menu_locations ) ) {
+	      $locations_assigned_to_this_menu = array();
+	      foreach ( array_keys( $menu_locations, $_nav_menu->term_id ) as $menu_location_key ) {
+	        if ( isset( $locations[ $menu_location_key ] ) ) {
+	          $locations_assigned_to_this_menu[] = $locations[ $menu_location_key ];
+	        }
+	      }
+
+	      /**
+	       * Filters the number of locations listed per menu in the drop-down select.
+	       *
+	       * @since 3.6.0
+	       *
+	       * @param int $locations Number of menu locations to list. Default 3.
+	       */
+	      $assigned_locations = array_slice( $locations_assigned_to_this_menu, 0, absint( apply_filters( 'wp_nav_locations_listed_per_menu', 3 ) ) );
+
+	      // Adds ellipses following the number of locations defined in $assigned_locations.
+	      if ( ! empty( $assigned_locations ) ) {
+	        printf( ' (%1$s%2$s)',
+	          implode( ', ', $assigned_locations ),
+	          count( $locations_assigned_to_this_menu ) > count( $assigned_locations ) ? ' &hellip;' : ''
+	        );
+	      }
+	    }
+	    ?>
+	  </option>
+	<?php endforeach; ?>
+	</select>
+	<?php
+}
+
+
+/*** MORE COMPLICATED VERSION ***/
+// https://wp.smashingmagazine.com/2011/10/create-custom-post-meta-boxes-wordpress/ 
 
 /*Registering the Left Sidebar
 register_sidebar(array(
